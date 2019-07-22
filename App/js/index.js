a5ad9c3659336dda07c9c8f12d388b387020b016
minghenudt <heming@mesall.cn>
2019-07-22 16:59:28 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,54 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.SVGPaper")
+                .setHost(host,"xui_ui_svgpaper1")
+                .setLeft("14.166666666666666em")
+                .setTop("10em")
+                .setWidth("44.5em")
+                .setHeight("36.666666666666664em")
+            );
+            
+            host.xui_ui_svgpaper1.append(
+                xui.create("xui.svg.circle")
+                .setHost(host,"xui_svg_circle1")
+                .setSvgTag("Shapes:Circle")
+                .setAttr({
+                    "r":20,
+                    "stroke":"#004A7F",
+                    "fill":"#ffffff",
+                    "stroke-width":2,
+                    "cx":150,
+                    "cy":110
+                })
+            );
+            
+            host.xui_ui_svgpaper1.append(
+                xui.create("xui.svg.path")
+                .setHost(host,"xui_svg_path1")
+                .setSvgTag("Shapes:Line")
+                .setAttr({
+                    "path":"M,170,120L,290,190",
+                    "stroke":"#004A7F",
+                    "fill":"#ffffff",
+                    "stroke-width":2
+                })
+            );
+            
+            host.xui_ui_svgpaper1.append(
+                xui.create("xui.svg.image")
+                .setHost(host,"xui_svg_image1")
+                .setAttr({
+                    "src":"{xui.ini.img_pic}",
+                    "width":100,
+                    "height":100,
+                    "x":290,
+                    "y":170
+                })
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
